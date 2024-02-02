55cb2ea7faa4aa67fd2719e6aedb55f5f45d6cad
7zhy64 <hong12603@gmail.com>
2024-02-02 11:57:44 +0800

    I am sorry I made silly mistake

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${PORT}`)
 })
