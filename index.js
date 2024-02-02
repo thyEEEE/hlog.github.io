18a56a515227905d62ec19e53ecbe6447a3efd3f
7zhy64 <hong12603@gmail.com>
2024-02-02 18:34:45 +0800

    Added my first blog

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/schedule', (req, res) => {
+    console.log("hello world")
+    res.sendFile(__dirname + "/schedule.html");
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/blog/my-first-blog', (req, res) => {
+    res.sendFile(__dirname + "/blog/my-first-blog.html");
+});
 
 app.use(express.static("."));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-})
-
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Example app listening on port ${PORT}`)
-})
+    console.log(`Example app listening on port ${PORT}`);
+});
