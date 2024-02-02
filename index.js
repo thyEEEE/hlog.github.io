0d1bc47ca55321d54a167d4fc6c8e76dfbffbfc9
7zhy64 <hong12603@gmail.com>
2024-02-02 11:46:41 +0800

    For running on railway

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Example app listening on port ${port}`)
 })
