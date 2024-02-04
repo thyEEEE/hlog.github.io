dc57e0417d7e15edf111f1ff24f332839b4ba688
7zhy64 <hong12603@gmail.com>
2024-02-04 15:38:24 +0800

    changed API

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const secret = {
-    url : "https://4682ec87-4ade-4c44-8c44-c8040dc13a7f-00-3tq02wghstal8.sisko.replit.dev/",
+    url : "https://api-production-5890.up.railway.app/",
     password : "32690757Thy!"
 }
 export default secret
